e5a1f21ca44456d9960ba284ae70a80d1582661e
kakau01 <leite5387@hotmail.com>
2021-02-18 08:31:48 -0300

    api connection and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-const routes = () => {
+import Dashboard from '../pages/Dashboard';
+import Error from '../pages/Error';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import RecoveryPass from '../pages/RecoveryPass';
+
+const Routes: React.FC = () => {
     return(
-        <h1>Rotas</h1>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/error" exact component={Error} />
+            <Route path="/pass" exact component={RecoveryPass} />
+            <Route path="/dashboard" exact component={Dashboard} />
+        </Switch>
+
     )
 }
 
-export default routes;
+export default Routes;
