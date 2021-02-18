98a2dc750acbc5dc9b511a99669acdcb07ea3254
kakau01 <leite5387@hotmail.com>
2021-02-18 08:02:23 -0300

    estrutura de arquivos e pastas

---
+++ src/pages/RecoveryPass/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RecoveryPass: React.FC = () => {
+    return(
+        <h1>RecoveryPass</h1>
+    )
+}   
+
+export default RecoveryPass;
---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const routes = () => {
+    return(
+        <h1>Rotas</h1>
+    )
+}
+
+export default routes;
